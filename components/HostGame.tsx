a7520d086509fa188eb2ef454e27b92d9e999895
Daniel Proode <daniel.proode@gmail.com>
2023-09-05 22:58:41 +0300

    Fixed type errors.

--- components/HostGame.tsx
+++ components/HostGame.tsx
@@ -1,38 +1,22 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import Button from './res/Button';
+import { ScrollView, StyleSheet, View } from 'react-native';
+import Selection from './Selection';
+import { StackNavigationProp } from '@react-navigation/stack';
 
+export type HostGameProps = {
+    navigation: StackNavigationProp<any>;
 
-function HostGame( { navigation }: {navigation: any}): JSX.Element {
+};
 
-    let gameCode = 0;
-
-    function generateCode() {
-        const randomInt = (min: number, max: number) =>
-        Math.floor(Math.random() * (max - min + 1)) + min;
-
-        // In the future, check if active game already exists with generated code
-
-        gameCode = randomInt(100000, 999999);
-    }
-
-    generateCode();
+const HostGame = ({ navigation }: HostGameProps) => {
 
   return (
     <View style={styles.joinGameView}>
-    <Text style={styles.roomCodeText}>ROOM CODE:</Text>
-    <Text style={styles.codeText}>#{gameCode}</Text>
-    <View style={styles.buttonView}>
-    <Button
-        onPress={ () =>
-            navigation.navigate('Lobby', {gameCode: gameCode}) }
-        text="HOST GAME"
-        />
+        <Selection gameCode={0} navigation={navigation} hostGame={true}/>
         </View>
-    </View>
 
   );
-}
+};
 
 
 const styles = StyleSheet.create({
@@ -40,32 +24,6 @@ const styles = StyleSheet.create({
         alignItems: 'center',
         backgroundColor: '#1E1E1E',
         flex: 1,
-    },
-    gameCodeInput: {
-        marginTop: 50,
-        width: 120,
-        borderWidth: 2,
-        borderRadius: 10,
-        padding: 10,
-        borderColor: 'gray',
-        textAlign: 'center',
-    },
-    roomCodeText: {
-        fontSize: 50,
-        marginTop: 200,
-        color: 'white',
-    },
-    codeText: {
-        fontSize: 40,
-        color: 'white',
-    },
-    buttonView: {
-        marginTop: 50,
-    },
-    startGameButton: {
-        padding: 10,
-        backgroundColor: '#DDDDDD',
-        marginTop: 50,
     },
   });
 export default HostGame;
