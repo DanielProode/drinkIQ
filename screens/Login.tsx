03664c058efe031b63caaa7775df99e78fec68ff
Karl Martin Uustalu <karlu@Karls-Air.lan>
2023-10-28 19:58:20 +0300

    Make Login page the default welcome page

--- screens/Login.tsx
+++ screens/Login.tsx
@@ -1,14 +1,25 @@
+import { NativeStackNavigationProp } from '@react-navigation/native-stack';
 import { useState } from 'react';
-import { View, Text, TextInput, StyleSheet, TouchableWithoutFeedback, Keyboard } from 'react-native';
+import { Image, View, Text, TextInput, StyleSheet, TouchableWithoutFeedback, Keyboard, Pressable } from 'react-native';
 
+import TermsAndConditions from './TermsAndConditions';
 import Button from '../components/Button';
 import { useAuth } from '../context/authContext';
 
-export default function Login() {
+interface LoginProps {
+  navigation: NativeStackNavigationProp<any>;
+}
+
+export default function Login({ navigation }: LoginProps) {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [error, setError] = useState('');
+  const [isModalVisible, setIsModalVisible] = useState(false);
   const { signIn } = useAuth();
+
+  const toggleModal = () => {
+    setIsModalVisible(!isModalVisible);
+  };
 
   const handleLogin = async () => {
     setError('');
@@ -22,6 +33,9 @@ export default function Login() {
   return (
     <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()} >
       <View style={styles.container}>
+        <TermsAndConditions isVisible={isModalVisible} onClose={toggleModal} />
+        <Image style={styles.cheersIcon}
+          source={require('../assets/images/cheers_icon.png')} />
         <Text style={styles.drinkIQLogo}>DRINKIQ</Text>
         <View style={styles.textInputContainer}>
           <TextInput
@@ -43,9 +57,26 @@ export default function Login() {
           />
         </View>
         <View style={styles.buttonContainer}>
-          <Button text='LOGIN' onPress={handleLogin} />
           {error ? <Text style={styles.error}>Login failed: {error}</Text> : null}
+          <Button text='LOGIN' onPress={handleLogin} />
+          <Pressable
+            onPress={() => navigation.navigate('Register')}>
+            <Text style={styles.signUpText}>Don't have an account?
+              <Text style={styles.innerText}> Sign Up</Text>
+            </Text>
+          </Pressable>
+          <Button
+            text="JOIN GAME AS GUEST"
+            buttonWidthNumber={1.3} />
         </View>
+        <Pressable
+          style={styles.touchableTerms}
+          onPress={toggleModal}>
+          <Text style={styles.outerText}>By signing up you agree with the
+            <Text
+              style={styles.innerText}> drinkIQ Terms and Conditions.</Text>
+          </Text>
+        </Pressable>
       </View>
     </TouchableWithoutFeedback>
   );
@@ -58,23 +89,24 @@ const styles = StyleSheet.create({
     backgroundColor: '#1E1E1E',
   },
   drinkIQLogo: {
-    marginTop: 100,
     fontFamily: 'Knewave',
-    fontSize: 40,
+    marginTop: 150,
+    fontSize: 60,
     color: 'white',
+  },
+  cheersIcon: {
+    position: 'absolute',
+    resizeMode: 'contain',
+    width: 250,
+    height: 250,
+    right: 20,
+    top: 20,
+    opacity: 0.3,
   },
   textInputContainer: {
-    marginTop: 150,
-    flex: 1,
+    marginTop: 100,
     width: '100%',
     alignItems: 'center',
-  },
-  title: {
-    fontSize: 32,
-    marginTop: 100,
-    marginBottom: 20,
-    color: 'white',
-    fontFamily: 'Basic',
   },
   input: {
     width: '80%',
@@ -89,11 +121,31 @@ const styles = StyleSheet.create({
     color: '#ABABAB',
   },
   buttonContainer: {
-    flex: 1.6,
     alignItems: 'center',
+    rowGap: 30,
+  },
+  touchableTerms: {
+    flex: 1,
+    alignItems: 'center',
+  },
+  innerText: {
+    fontFamily: 'Cabin-Bold',
+    color: 'white',
+  },
+  outerText: {
+    position: 'absolute',
+    bottom: 20,
+    width: 300,
+    textAlign: 'center',
+    marginBottom: 20,
+    color: '#FFFFFF95',
+    fontFamily: 'Cabin-Regular',
+  },
+  signUpText: {
+    color: '#FFFFFF95',
+    fontFamily: 'Cabin-Regular',
   },
   error: {
     color: 'red',
-    marginTop: 10,
   },
 });
